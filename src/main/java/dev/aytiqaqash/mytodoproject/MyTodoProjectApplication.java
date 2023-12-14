282d9fce2337549afafa4259afe09bd29385ef5b
aytiqaqash <aytiqaqash@gmail.com>
2023-12-13 20:03:11 -0500

    Step 1. Create a project Description: Open your preferred IDE (Integrated Development Environment) and create a new project. Select “Spring Initializr” as the project type. Choose “Maven” as the build tool. Select “Java” as the language. Choose the latest version of Spring Boot (3.2.0) from the dropdown menu. Add the following dependencies: Spring Web Spring Data JPA PostgreSQL Driver Click “Generate” to create the project.

---
+++ src/main/java/dev/aytiqaqash/mytodoproject/MyTodoProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.aytiqaqash.mytodoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTodoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTodoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/aytiqaqash/mytodoproject/MyTodoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aytiqaqash.mytodoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
