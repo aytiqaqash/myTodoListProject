9bdaeac9cfc8f0555536cbc11867fe25b506a707
aytiqaqash <aytiqaqash@gmail.com>
2023-12-13 20:07:20 -0500

    Step 3. Add TodoRepository interface Description: Create a new package named “repository” in your project. Create a new interface named “TodoRepository” in the “repository” package. Extend the “JpaRepository” interface. Add the “Todo” class and the “id” field as type arguments to the “JpaRepository” interface. Annotate the “TodoRepository” interface with @Repository.

---
+++ src/main/java/dev/aytiqaqash/mytodoproject/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package dev.aytiqaqash.mytodoproject.repository;
+
+import dev.aytiqaqash.mytodoproject.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
